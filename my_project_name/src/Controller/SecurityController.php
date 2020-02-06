4d846c2fda9f51e06237192fb23f8908f8451e70
MarcGlug <marc.boyon@hotmail.fr>
2020-02-06 16:47:45 +0100

    nouvelle base (normalement solide)

---
+++ my_project_name/src/Controller/SecurityController.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Users;
+use App\Form\RegistrationFormType;
+use App\Service\EmailService;
+use App\Security\UsersAuthenticator;
+use App\Repository\UsersRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UsersAuthenticator $authenticator, EmailService $emailService): Response
+    {
+        $user = new Users();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $now = new \DateTime();
+            $user
+                -> setToken($this->generateToken())
+                -> setActive(0)
+            ;
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $emailService->register($user);
+
+           $this->addFlash('success', "Inscription bien prise en compte. Clique sur le lien wesh envoyé à l'email ".$user->getEmail());
+           return $this->redirectToRoute('app_login');
+
+            // return $guardHandler->authenticateUserAndHandleSuccess(
+            //     $user,
+            //     $request,
+            //     $authenticator,
+            //     'main' // firewall name in security.yaml
+            // );
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('accueil');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    } 
+
+    /**
+     * @Route("/validation-email", name="validation_email")
+     */
+    public function validation_email(Request $request, UsersRepository $usersRepo)
+    {   $error = false;
+        //récupération GET email -token
+        $email = $request->query->get('email');
+        $token = $request->query->get('token');
+        //chercher le usen BDD par son email
+        $user = $usersRepo->findOneBy(array('email' =>$email));
+
+       
+
+            if(! $user){
+            $error = "Votre adresse mail ne correspond à aucun compte";
+            }elseif($user->getActive() == 1){
+            $error = "Compte déjà valider wesh! ";
+            }elseif($token != $user->getToken()){
+            $error = "Une erreur est survenue. Contacter notre service com";
+            }else{
+            $user->setActive(1);
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $this->addFlash('success','Ton compte a bien été validé, tu peux te connecter');
+            return $this->redirectToRoute('app_login');
+            }
+        
+        
+        return $this->render('emails/security/validation_emaill.html.twig', [
+            'error' => $error,
+        ]);
+    }
+
+    private function generateToken(){
+        return bin2hex(random_bytes(21));
+    }
+
+    /**
+     * @Route("/password-forgotten", name="password_forgotten")
+     */
+    public function password_forgotten(Request $request, UsersRepository $usersRepo, EmailService $emailService){
+        if($request->isMethod('POST')){
+            $email = $request->request->get('email');
+            $user = $usersRepo->findOneBy( array('email'=> $email));
+            
+            if(!$user){
+                $this->addFlash('danger', 'Adresse mail non trouvé');
+            }else{
+                $user->setToken($this->generateToken());
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+
+                $link = $this->generateUrl('password_update', ['email' => $user->getEmail(), 'token' => $user->getToken()],UrlGeneratorInterface::ABSOLUTE_URL );
+                $emailService->password_forgotten($user, $link);
+                $this->addFlash('success', "On t'as envoyer un email, va vite voir!");
+                return $this->redirectToRoute('password_forgotten', ['send' => 'ok']);
+            }
+        }
+        return $this->render('security/password_forgotten.html.twig');
+    }
+    
+    /**
+     * @Route("/password-update", name="password_update")
+     */
+    public function password_update(Request $request, UsersRepository $usersRepo, UserPasswordEncoderInterface $passwordEncoder){
+        $user = $this->getUser();
+        if(!$user){
+            $email = $request->query->get('email');
+            $token = $request->query->get('token');
+        
+            $user = $usersRepo->findOneBy( array('email' => $email));  
+        
+            if(!$user || $token != $user->getToken()){
+                throw new \Exception("Hola mon mignon! Tu t'es égaré!");
+            }
+        }
+
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form 
+            ->remove('name')
+            ->remove('lastname')
+            ->remove('email')
+            ->remove('agreeTerms');
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user
+                ->setToken($this->generateToken())
+                ->setUpdatedAt( new \DateTime());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $this->addFlash('success', "Ton mot de passe a bien été modifier");
+            return $this->redirectToRoute('app_login');
+        }
+        
+        return $this->render('security/password_update.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ my_project_name/src/Entity/Annonces.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AnnoncesRepository")
+ */
+class Annonces
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_creation;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_limite;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $active;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\users", inversedBy="annonces")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+    public function getDateLimite(): ?\DateTimeInterface
+    {
+        return $this->date_limite;
+    }
+
+    public function setDateLimite(?\DateTimeInterface $date_limite): self
+    {
+        $this->date_limite = $date_limite;
+
+        return $this;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive(int $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getUser(): ?users
+    {
+        return $this->user;
+    }
+
+    public function setUser(?users $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
