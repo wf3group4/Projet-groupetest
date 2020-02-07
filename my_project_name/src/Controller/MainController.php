bb82753c2b61a89a36dd7491f72a5a9389e96d27
MarcGlug <marc.boyon@hotmail.fr>
2020-02-07 13:01:38 +0100

    test

--- my_project_name/src/Controller/MainController.php
+++ my_project_name/src/Controller/MainController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\UsersRepository;
 
 class MainController extends AbstractController
 {
@@ -13,6 +15,31 @@ class MainController extends AbstractController
     public function index()
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    
+    /**
+     * @Route("/mon-compte", name="mon_compte")
+     */
+    public function mon_compte()
+    {
+        return $this->render('main/mon_compte.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+     
+    /**
+     * @Route("/modification-compte/{name}", name="modif_compte")
+     */
+    public function modif_compte($name, Request $request, UsersRepository $usersRepo )
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $usersRepo->find($name); 
+       
+        return $this->render('main/modif_compte.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
